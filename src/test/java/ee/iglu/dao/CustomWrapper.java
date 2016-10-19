86951433000229cbb4180f5a80ddb1fe37457be4
Meelis Lehtmets <meelis.lehtmets@iglu.ee>
2016-10-19 10:09:27 +0300

    more verbose test error messages

--- src/test/java/ee/iglu/dao/CustomWrapper.java
+++ src/test/java/ee/iglu/dao/CustomWrapper.java
@@ -3,8 +3,10 @@ package ee.iglu.dao;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @Getter
+@ToString
 @EqualsAndHashCode
 @RequiredArgsConstructor
 public class CustomWrapper {
